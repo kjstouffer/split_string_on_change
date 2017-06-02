86a16c787e4ec998c9c5a804c1d2a9f47cad8034
Keith Stouffer <kjstouffer@gmail.com>
2017-06-02 09:14:33 -0400

    fixed two edge-cases: lengths 0 and 1

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,11 @@ fn splitter(string: &str) -> String {
     let chars: Vec<char> = string.chars().collect();
     let mut result = Vec::<String>::new();
     let mut last_mismatch = 0;
-    for i in 1..chars.len() {
-        if chars[i-1] != chars[i] {
+    for i in 0..chars.len() {
+        if chars.len() == 1 {
+            return chars[0..1].iter().map(|c| c ).collect();
+        }
+        if i > 0 && chars[i-1] != chars[i] {
             let temp_result: String = chars[last_mismatch..i].iter().map(|c| c).collect();
             result.push( temp_result );
             last_mismatch = i;
@@ -17,8 +20,13 @@ fn splitter(string: &str) -> String {
 }
 
 fn main() {
+    let test_string = "g";
+    println!("input string: {}", test_string);
+    println!("output string: {}", splitter(test_string));
+    let test_string = "";
+    println!("input string: {}", test_string);
+    println!("output string: {}", splitter(test_string));
     let test_string = "gHHH5YY++///\\";
-
     println!("input string: {}", test_string);
     println!("output string: {}", splitter(test_string));
 }
